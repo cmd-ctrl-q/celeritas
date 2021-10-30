59993bd494d6b6f9e18043c492186572606f254c
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-10-29 23:45:57 -0500

    write more tests for render package

--- render/render_test.go
+++ render/render_test.go
@@ -18,12 +18,66 @@ func TestRender_Page(t *testing.T) {
 	testRenderer.Renderer = "go"
 	testRenderer.RootPath = "./testdata"
 
+	// test existing page
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error(err)
 	}
 
+	// test non-existing go page
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("Error rendering non-existing go template", err)
+	}
+
 	testRenderer.Renderer = "jet"
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// test non-existing jet page
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("Error rendering non-existing jet template", err)
+	}
+
+	// default case, no engine specified
+	testRenderer.Renderer = ""
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err == nil {
+		t.Error("No error returned while rendering with invalid renderer specified", err)
+	}
+
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./testdata"
+
+	// test existing page
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+
+	// test existing page
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error(err)

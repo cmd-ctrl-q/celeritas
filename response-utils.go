1408e6df429fecc7c9ecd9126abfd5341a4bda4a
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-11-10 11:54:01 -0600

    create read json method

--- response-utils.go
+++ response-utils.go
@@ -3,11 +3,35 @@ package celeritas
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"path"
 	"path/filepath"
 )
+
+func (c *Celeritas) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	// sanity check
+	maxBytes := 1048571 // one megabyte
+	// read up to one megabyte from the body
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	// only allow one entry
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only have a single json value")
+	}
+
+	return nil
+}
 
 func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
 	out, err := json.MarshalIndent(data, "", "\t")

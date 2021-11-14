e16ee8d7eb8545ac83312284d4462730179e5cb1
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-11-13 23:41:21 -0600

    add mailing packages and complete sending email via smtp

--- mailer/mail.go
+++ mailer/mail.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"text/template"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
 )
 
 type Mail struct {
@@ -13,7 +17,7 @@ type Mail struct {
 	// Templates holds the path to the templates directory
 	Templates   string
 	Host        string
-	Port        string
+	Port        int
 	Username    string
 	Password    string
 	Encryption  string
@@ -49,6 +53,7 @@ func (m *Mail) ListenForMail() {
 	for {
 		// listen for incoming messages on the jobs channel
 		msg := <-m.Jobs
+		// try sending message
 		err := m.Send(msg)
 		if err != nil {
 			// send error to results
@@ -66,7 +71,69 @@ func (m *Mail) Send(msg Message) error {
 
 func (m *Mail) SendSMTPMessage(msg Message) error {
 
+	// generate plain text and formatted message
+	formattedMessage, err := m.buildHTMLMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	plainMessage, err := m.buildPlainTextMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	// mailer
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	// create client to connect to server
+	smtpClient, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+	// method chain
+	email.SetFrom(msg.From).
+		AddTo(msg.To).
+		SetSubject(msg.Subject)
+	email.SetBody(mail.TextHTML, formattedMessage)
+	email.AddAlternative(mail.TextPlain, plainMessage)
+
+	// check if any attachments
+	if len(msg.Attachments) > 0 {
+		for _, att := range msg.Attachments {
+			email.AddAttachment(att)
+		}
+	}
+
+	// send message
+	err = email.Send(smtpClient)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (m *Mail) getEncryption(e string) mail.Encryption {
+	switch e {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSL
+	case "none":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
 }
 
 func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
@@ -83,6 +150,12 @@ func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
 	}
 
 	formattedMessage := tpl.String()
+
+	// inline the css
+	formattedMessage, err = m.inlineCSS(formattedMessage)
+	if err != nil {
+		return "", err
+	}
 
 	return formattedMessage, nil
 }
@@ -103,4 +176,25 @@ func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
 	plainMessage := tpl.String()
 
 	return plainMessage, nil
+}
+
+func (m *Mail) inlineCSS(s string) (string, error) {
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+
+	// create html
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
 }

1a47bdeb8c96ed357dd1b3bf76e44359b1649bab
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-11-24 16:08:12 -0600

    create go.mod.txt; read and update go.mod

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -12,8 +12,11 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
+var appURL string
+
 func doNew(appName string) {
 	appName = strings.ToLower(appName)
+	appURL = appName
 
 	// sanitize application name (convert url to single word)
 	if strings.Contains(appName, "/") {
@@ -97,6 +100,21 @@ func doNew(appName string) {
 	_ = os.Remove("./" + appName + "/Makefile.windows")
 
 	// update the go.mod file
+	color.Yellow("\tCreating go.mod file...")
+	_ = os.Remove("./" + appName + "/go.mod")
+
+	data, err = templateFS.ReadFile("templates/go.mod.txt")
+	if err != nil {
+		exitGraceFully(err)
+	}
+
+	mod := string(data)
+	mod = strings.ReplaceAll(mod, "${APP_NAME}", appURL)
+
+	err = copyDataToFile([]byte(mod), "./"+appName+"/go.mod")
+	if err != nil {
+		exitGraceFully(err)
+	}
 
 	// update existing .go files with correct name/imports
 
